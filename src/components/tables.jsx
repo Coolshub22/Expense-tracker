dd35962c98928b80c9117244b7d54424b1103f7a
Arnold <arnoldkulavi@gmail.com>
2025-04-13 22:54:43 +0300

    feat:add tables in tables.jsx

--- src/components/tables.jsx
+++ src/components/tables.jsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function Tables() {
   return (
-    <div>Tables</div>
+    <div>
+        <h1>Tables</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>Expense</th>
+                    <th>Description</th>
+                    <th>Category</th>
+                    <th>Amount</th>
+                    <th>Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Groceries</td>
+                    <td>Kales</td>
+                    <td>Grocery</td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Food</td>
+                    <td>Chapo</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
   )
 }
